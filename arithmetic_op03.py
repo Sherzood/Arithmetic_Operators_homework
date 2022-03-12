66a3773f3cf0e1f5ef483efcdcf5fdacf96135bc
sherzod89 <sherzodms@inbox.ru>
2022-03-12 15:41:43 +0300

    01

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,8 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number=12
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+n1=number%10
+n2=number//10
+answer=(n1*10)+n2
 #Print the answer variable
+print(answer)
